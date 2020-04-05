8c9af84f26e4a4415c2c33d20643719b24a97144
pambrose <pambrose@mac.com>
2020-04-05 15:38:56 -0700

    Update docs

--- my_code.py
+++ my_code.py
@@ -4,3 +4,7 @@ def double_it(x):
 
 if __name__ == '__main__':
     print(double_it(6))
+
+    # After you are satisfied with your results, use input() to prompt the user for a value:
+    # v = input("Value to double: ")
+    # print(inc(double_it(v)))
