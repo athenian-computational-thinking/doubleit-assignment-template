65d4b7e0125dc94da6f87b5be91250e4ebb27394
Megan Leich <mleich@athenian.org>
2020-04-14 16:52:37 +0000

    reset

--- my_code.py
+++ my_code.py
@@ -1,11 +1,11 @@
 def double_it(x):
-    product = x*2
-    return  product
+    
+    return  # add variable to return here
 
 
 if __name__ == '__main__':
     print(double_it(6))
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
-     v = input("Value to double: ")
-     print(inc(double_it(v)))
+    #v = input("Value to double: ")
+    #print(inc(double_it(v)))
