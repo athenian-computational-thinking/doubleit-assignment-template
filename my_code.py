407a4051524dd3d01240dfeb0a2396a3890974ba
Megan Leich <mleich@athenian.org>
2020-04-14 17:42:57 +0000

    added code to double

--- my_code.py
+++ my_code.py
@@ -1,11 +1,11 @@
 def double_it(x):
     
-    return  # add variable to return here
+    return  x*2
 
 
 if __name__ == '__main__':
-    print(double_it(6))
+    #print(double_it(6))
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to double: ")
-    #print(inc(double_it(v)))
+    v = int(input("Value to double: "))
+    print((double_it(v)))
